c152b4f6882fa1e40bb2bec2bc58f891ae80df6f
Olivér Halász <oliver.viktor.halasz@gmail.com>
2021-05-16 21:37:43 +0200

    Added missing CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { json } from "body-parser";
+import cors from "cors";
 import "express-async-errors";
 import { getCocktailRouter } from "./routes/getCocktail";
 
@@ -7,12 +8,20 @@ const app = express();
 
 // Parsing req as json
 app.use(json());
+// Enabling CORS for the frontend
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
 
+// Attaching /cocktail route
 app.use(getCocktailRouter);
 
 // Invalid route
 app.all("*", async (req, res) => {
-  throw new Error("Route not found");
+  res.status(404).send("Route not found");
 });
 
 export default app;
